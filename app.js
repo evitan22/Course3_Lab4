7b3af7a52d1b2ae3cf46e240958b3757ff57ce3c
evitan22 <kokravets@gmail.com>
2026-05-16 19:14:22 +0300

    part 1

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+//Morgan
+const morgan = require('morgan');
+app.use(morgan('combined'));
+
+//Winston
+const winston = require('winston');
+const logger = winston.createLogger({
+level: 'info',
+transports: [
+new winston.transports.File({ filename: 'app.log' })
+]
+});
+logger.info('Server started');
+
+app.get('/', (req, res) => {
+    res.send('Server is running');
+});
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+});
